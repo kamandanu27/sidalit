e18206eaeb4252373481ef47a72bb1295cc2f4dd
matakoding <matakoding@gmail.com>
2023-05-07 00:46:16 +0700

    update data

--- application/views/cabor/v_content.php
+++ application/views/cabor/v_content.php
@@ -1,11 +1,11 @@
 <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Data Tables</h1>
+      <h1>Data Cabor</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item">Tables</li>
+          <li class="breadcrumb-item">Cabor</li>
           <li class="breadcrumb-item active">Data</li>
         </ol>
       </nav>
@@ -17,58 +17,35 @@
 
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Datatables</h5>
+              <h5 class="card-title">Datacabor</h5>
               <p>Add lightweight datatables to your project with using the <a href="https://github.com/fiduswriter/Simple-DataTables" target="_blank">Simple DataTables</a> library. Just add <code>.datatable</code> class name to any table you wish to conver to a datatable</p>
 
               <!-- Table with stripped rows -->
-              <table class="table datatable">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Position</th>
-                    <th scope="col">Age</th>
-                    <th scope="col">Start Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Brandon Jacob</td>
-                    <td>Designer</td>
-                    <td>28</td>
-                    <td>2016-05-25</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Bridie Kessler</td>
-                    <td>Developer</td>
-                    <td>35</td>
-                    <td>2014-12-05</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Ashleigh Langosh</td>
-                    <td>Finance</td>
-                    <td>45</td>
-                    <td>2011-08-12</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>Angus Grady</td>
-                    <td>HR</td>
-                    <td>34</td>
-                    <td>2012-06-11</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">5</th>
-                    <td>Raheem Lehner</td>
-                    <td>Dynamic Division Officer</td>
-                    <td>47</td>
-                    <td>2011-04-19</td>
-                  </tr>
-                </tbody>
-              </table>
+              <table id="example1" class="table table-bordered table-hover">
+								<thead>
+									<tr>
+										<th>No</th>
+										<th>Nama Cabor</th>
+										<th>Aksi</th>
+									</tr>
+								</thead>
+								<tbody style="font-weight: 500;">
+                                    <?php $no=1; foreach($tabel as $row){ ?>
+                                        <tr>
+											<td><?= $no++ ?></td>
+											<td><?= $row->nama_cabor ?></td>
+											<td>
+
+												<a href="<?= base_url(); ?>cabor/edit/<?= $row->id_cabor ?>" type="submit" class="btn btn-warning" ><i class="icofont icofont-edit"></i> Edit</a>
+
+												<a type="submit" class="btn btn-danger btnhapus" data-id="<?= $row->id_cabor ?>" style="color: white;"><i class="icofont icofont-trash"></i> Hapus</a>
+
+
+											</td>
+                                        </tr>
+                                    <?php } ?>
+								</tbody>
+							</table>
               <!-- End Table with stripped rows -->
 
             </div>
